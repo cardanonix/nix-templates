0e1526453e5b7334d64f7c3675bb0efd3977f0cc
harryprayiv <harryprayiv@gmail.com>
2023-03-10 08:49:00 -0500

    morphed into IOG's flake

--- plutus/flake.nix
+++ plutus/flake.nix
@@ -21,7 +21,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, haskell-nix, CHaP, iohk-nix, ... }@context:
+  outputs = { self, nixpkgs, flake-utils, haskell-nix, CHaP, iohk-nix, ... }:
 
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
@@ -35,14 +35,16 @@
         };
 
         inherit (pkgs) lib;
-        hixProject = pkgs.haskell-nix.hix.project {
+        project = pkgs.haskell-nix.cabalProject' {
           src = ./.;
-          evalSystem = "x86_64-linux";
-          inputMap = { 
-            "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP; 
+          compiler-nix-name = "ghc8107";
+
+          inputMap = {
+            "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
           };
+
           shell.tools = {
-            # cabal.version = "latest";
+            cabal.version = "latest";
             hlint.version = "3.4.1";
             haskell-language-server.version = "1.8.0.0";
           };
@@ -59,34 +61,14 @@
               };
             }
           ];
+
         };
-          hixFlake = hixProject.flake { };
-          serve-docs = import ./nix/serve-docs.nix context {
-            inherit hixProject;
-            # TODO transform additionalPkgs in excludePkgs to reduce boilerplate
-            #  we could collect all entries from cabal build-depends
-            #  (maybe through hixProject.hsPkgs)
-            additionalPkgs = [ "cardano-api" ];
-          };
-        in
-        # Flake definition follows hello.cabal
-        {
-          inherit (hixFlake) apps checks system;
-          legacyPackages = pkgs;
 
-          packages = hixFlake.packages // {
-            inherit serve-docs;
-          };
+        flake = project.flake { };
 
-          devShell = pkgs.mkShell {
-            inputsFrom = [
-              hixFlake.devShell
-            ];
-            buildInputs = [
-              self.packages.${system}.serve-docs
-            ];
-          };
-        });
+      in
+      flake
+    );
 
   # --- Flake Local Nix Configuration ----------------------------
   nixConfig = {
@@ -94,20 +76,12 @@
     # Nix should ask for permission before using it,
     # but remove it here if you do not want it to.
     extra-substituters = [
-      "https://klarkc.cachix.org?priority=99"
       "https://cache.iog.io"
-      "https://iohk.cachix.org"
       "https://cache.zw3rk.com"
-      "https://cache.nixos.org"
-      "https://public-plutonomicon.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="
     ];
     allow-import-from-derivation = "true";
   };
