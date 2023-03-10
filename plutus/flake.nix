ee599c79ef1ab76135b16d34fb031edd8788908a
harryprayiv <harryprayiv@gmail.com>
2023-03-09 22:01:57 -0500

    crazy sweeping changes that can't possibly work

--- plutus/flake.nix
+++ plutus/flake.nix
@@ -1,44 +1,66 @@
 {
   # This is a template created by `hix init`
-  inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
-  inputs.iohk-nix.url = "github:input-output-hk/iohk-nix";
-  inputs.CHaP.url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
-  inputs.CHaP.flake = false;
+  inputs = {
+    nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    flake-utils.follows = "haskell-nix/flake-utils";
+    iohk-nix.url = "github:input-output-hk/iohk-nix";
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
   # to generate docs
-  inputs.plutus.url = "github:input-output-hk/plutus";
-  inputs.utils.url = "github:ursi/flake-utils";
-  outputs = { self, utils, ... }@inputs:
-    utils.apply-systems
-      {
-        inherit inputs;
-        # see our haskell-nix
-        systems = [
-          "aarch64-linux"
-          "x86_64-linux"
-        ];
-        overlays = [
-          inputs.haskell-nix.overlay
-          # plutus runtime dependency
-          inputs.iohk-nix.overlays.crypto
-        ];
-      }
-      ({ pkgs, system, ... }@context:
-        let
-          hixProject = pkgs.haskell-nix.hix.project {
-            src = ./.;
-            evalSystem = "x86_64-linux";
-            inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.CHaP; };
-            modules = [
-              (_: {
-                # See input-output-hk/iohk-nix#488
-                packages.cardano-crypto-praos.components.library.pkgconfig =
-                  pkgs.lib.mkForce [ [ pkgs.libsodium-vrf ] ];
-                packages.cardano-crypto-class.components.library.pkgconfig =
-                  pkgs.lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 ] ];
-              })
-            ];
+    plutus.url = "github:input-output-hk/plutus";
+    # utils.url = "github:ursi/flake-utils";
+    iohk-nix = {
+      url = "github:input-output-hk/iohk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskell-nix, CHaP, iohk-nix, ... }:
+
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskell-nix) config;
+          overlays = [
+            haskell-nix.overlay
+            iohk-nix.overlays.crypto
+          ];
+        };
+
+        inherit (pkgs) lib;
+
+        hixProject = pkgs.haskell-nix.hix.project {
+          src = ./.;
+          evalSystem = "x86_64-linux";
+          inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.CHaP; };
+          
+          shell.tools = {
+            cabal.version = "latest";
+            hlint.version = "3.4.1";
+            haskell-language-server.version = "1.8.0.0";
           };
+          
+          modules = [
+            {
+              packages = {
+                # Broken due to haddock errors. Refer to https://github.com/input-output-hk/plutus/blob/master/nix/pkgs/haskell/haskell.nix
+                plutus-ledger.doHaddock = false;
+                plutus-use-cases.doHaddock = false;
+
+                cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 ] ];
+                cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 ] ];
+              };
+            }
+          ];
+        };
           hixFlake = hixProject.flake { };
           serve-docs = import ./nix/serve-docs.nix inputs context {
             inherit hixProject;
